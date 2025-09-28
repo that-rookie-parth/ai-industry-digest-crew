8dac54723f7c49ff544d177973e121d53ed9671f
Parth Kulshreshtha <kulshreshthaparth@gmail.com>
2025-09-28 13:56:55 +0530

    updated the task, agent and crew defination according to the broader goal

--- src/multi_platform_content_repurposing_crew/crew.py
+++ src/multi_platform_content_repurposing_crew/crew.py
@@ -3,10 +3,7 @@ from typing import List
 from crewai import Agent, Crew, Process, Task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from crewai.project import CrewBase, agent, crew, task
-
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+from crewai_tools import TavilySearchTool
 
 
 @CrewBase
@@ -16,29 +13,30 @@ class MultiPlatformContentRepurposingCrew:
     agents: List[BaseAgent]
     tasks: List[Task]
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
     def researcher(self) -> Agent:
         return Agent(
             config=self.agents_config["researcher"],  # type: ignore[index]
             verbose=True,
+            tools=[
+                TavilySearchTool(),
+            ],
         )
 
     @agent
-    def reporting_analyst(self) -> Agent:
+    def analyst(self) -> Agent:
         return Agent(
-            config=self.agents_config["reporting_analyst"],  # type: ignore[index]
+            config=self.agents_config["analyst"],  # type: ignore[index]
             verbose=True,
         )
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+    @agent
+    def writer(self) -> Agent:
+        return Agent(
+            config=self.agents_config["writer"],  # type: ignore[index]
+            verbose=True,
+        )
+
     @task
     def research_task(self) -> Task:
         return Task(
@@ -46,22 +44,24 @@ class MultiPlatformContentRepurposingCrew:
         )
 
     @task
-    def reporting_task(self) -> Task:
+    def analysis_task(self) -> Task:
         return Task(
-            config=self.tasks_config["reporting_task"],  # type: ignore[index]
-            output_file="report.md",
+            config=self.tasks_config["analysis_task"],  # type: ignore[index]
+        )
+
+    @task
+    def report_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["report_task"],  # type: ignore[index]
         )
 
     @crew
     def crew(self) -> Crew:
         """Creates the MultiPlatformContentRepurposingCrew crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
 
         return Crew(
-            agents=self.agents,  # Automatically created by the @agent decorator
-            tasks=self.tasks,  # Automatically created by the @task decorator
+            agents=self.agents,
+            tasks=self.tasks,
             process=Process.sequential,
             verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )

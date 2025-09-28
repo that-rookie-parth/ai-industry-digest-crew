1b6c9e33567ac47673566fa72efb39db004eb9b0
Parth Kulshreshtha <kulshreshthaparth@gmail.com>
2025-09-28 13:57:26 +0530

    basic cleaning and input changes

--- src/multi_platform_content_repurposing_crew/main.py
+++ src/multi_platform_content_repurposing_crew/main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import sys
 import warnings
 from datetime import datetime
@@ -9,17 +8,12 @@ from multi_platform_content_repurposing_crew.crew import (
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
-
 
 def run():
     """
     Run the crew.
     """
-    inputs = {"topic": "AI LLMs", "current_year": str(datetime.now().year)}
+    inputs = {"date": str(datetime.today().date().isoformat())}
 
     try:
         MultiPlatformContentRepurposingCrew().crew().kickoff(inputs=inputs)
